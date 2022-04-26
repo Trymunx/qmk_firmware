127e895753f243f0e0454945646a3078c5081839
Alex Houseago <ahouseago@gmail.com>
2022-04-26 22:28:40 +0100

    Fixes, including better default backlighting

--- keyboards/hs60/v2/iso/keymaps/ahouseago/config.h
+++ keyboards/hs60/v2/iso/keymaps/ahouseago/config.h
@@ -2,3 +2,20 @@
 
 #define TAPPING_TERM 200
 #define RETRO_TAPPING
+
+#undef RGB_BACKLIGHT_DISABLE_WHEN_USB_SUSPENDED
+#define RGB_BACKLIGHT_DISABLE_WHEN_USB_SUSPENDED 1
+
+// the default brightness
+#undef RGB_BACKLIGHT_BRIGHTNESS
+#define RGB_BACKLIGHT_BRIGHTNESS 80
+
+// the default effect (RGB test)
+#undef RGB_BACKLIGHT_EFFECT
+#define RGB_BACKLIGHT_EFFECT 1
+
+// the default color1 and color2
+#undef RGB_BACKLIGHT_COLOR_1
+#undef RGB_BACKLIGHT_COLOR_2
+#define RGB_BACKLIGHT_COLOR_1 { .h = 32, .s = 180 }
+#define RGB_BACKLIGHT_COLOR_2 { .h = 32, .s = 180 }
