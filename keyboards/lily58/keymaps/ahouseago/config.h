7e390ed3768908a6d5ec6f4c3c11642f16f7c113
Alex Houseago <ahouseago@gmail.com>
2022-10-18 02:06:26 +0100

    Some more updates to the lily58 config

--- keyboards/lily58/keymaps/ahouseago/config.h
+++ keyboards/lily58/keymaps/ahouseago/config.h
@@ -26,9 +26,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-// #define TAPPING_FORCE_HOLD
+#define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 200
-#define RETRO_TAPPING
+#define RETRO_TAPPING_PER_KEY
 
 #undef RGBLED_NUM
 // #define RGBLIGHT_ANIMATIONS
--- keyboards/lily58/keymaps/ahouseago/keymap.c
+++ keyboards/lily58/keymaps/ahouseago/keymap.c
@@ -2,9 +2,10 @@
 
 enum layer_number {
   _COLEMAK = 0,
+  _QWERTY,
   _LOWER,
   _RAISE,
-  _QWERTY,
+  _UPPER,
   /* _ADJUST, */
 };
 
@@ -41,10 +42,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_COLEMAK] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
-  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_R,    KC_B,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,  KC_MINS,
+  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,  KC_MINS,
   HYP_ESC,  KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                     KC_M,    KC_N,    KC_E,    KC_I,    KC_O,     KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V,TO(_QWERTY),KC_NUBS, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                        KC_LCTL, KC_LGUI, MO(_LOWER), KC_BSPC, KC_ENT, KC_SPC, MO(_RAISE), KC_LALT
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V,DF(_QWERTY),KC_NUBS, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+                        KC_LCTL, KC_LGUI, MO(_LOWER), KC_BSPC, KC_ENT,LT(_UPPER,KC_SPC), MO(_RAISE), KC_LALT
+),
+/* QWERTY
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LCtl | LGUI |LOWER | /BckSpac/       \Enter \  |Space |RAISE | LAlt |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+
+ [_QWERTY] = LAYOUT(
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+  KC_CAPS,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,DF(_COLEMAK),KC_RBRC,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+                        KC_LCTL, KC_LALT, KC_SPC, KC_BSPC, KC_ENT, KC_SPC, MO(_RAISE), KC_LALT
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -62,9 +85,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
+  KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_NUHS,
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_COLN,
-  _______, MOD_UND, MOD_CUT, MOD_CPY, MOD_EXT, MOD_PST,LCTL(KC_B),_______,KC_TILD, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+  _______, MOD_UND, MOD_CUT, MOD_CPY, MOD_EXT, MOD_PST,LCTL(KC_B),_______,LSFT(KC_NUHS), KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
                              _______, _______, _______, _______, _______,  _______, _______, _______
 ),
 /* RAISE
@@ -89,27 +112,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, KC_LBRC, KC_RBRC, _______,  _______, KC_MPLY,  KC_PLUS, KC_MINS, KC_LBRC, KC_RBRC, KC_EQL,  KC_BSLS,
                              _______, _______, _______,  KC_DEL,  _______,  _______, _______, _______
 ),
-/* QWERTY
+/* UPPER
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |PrtScr|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |                    | Prev |VolDwn|VolUp | Next | Mute |AltIns|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |-------.    ,-------| Left | Down |  Up  |Right | PTT  |AltHom|
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------|   +  |   -  |   [  |   ]  |   =  |   \  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LCtl | LGUI |LOWER | /BckSpac/       \Enter \  |Space |RAISE | LAlt |
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
- [_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_CAPS,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,TO(_COLEMAK),KC_RBRC,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                        KC_LCTL, KC_LALT, KC_SPC, KC_BSPC, KC_ENT, KC_SPC, MO(_RAISE), KC_LALT
+[_UPPER] = LAYOUT(
+  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, KC_PSCR,
+  _______, _______, _______, KC_LCBR, KC_RCBR, KC_COLN,                     KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MUTE, ALT_INS,
+  _______, _______, _______, KC_LPRN, KC_RPRN, KC_EQL,                      LCTL(KC_H),LCTL(KC_J),LCTL(KC_K),LCTL(KC_L), PTT_RET, ALT_HOM,
+  _______, _______, _______, KC_LBRC, KC_RBRC, _______,  _______, KC_MPLY,  KC_PLUS, KC_MINS, KC_LBRC, KC_RBRC, KC_EQL,  KC_BSLS,
+                             _______, _______, _______,  KC_DEL,  _______,  _______, _______, _______
 )
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -174,6 +197,16 @@ bool oled_task_user(void) {
     return false;
 }
 #endif // OLED_ENABLE
+
+bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LT(_UPPER, KC_SPC):
+            return true;
+        default:
+            return false;
+    }
+}
+
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
