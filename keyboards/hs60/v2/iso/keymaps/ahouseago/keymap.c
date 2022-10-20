b487485bc4c184aaa4617f579e28acc033a67c96
Alex Houseago <ahouseago@gmail.com>
2022-10-21 00:40:37 +0100

    Add a colemak layer to HS60

--- keyboards/hs60/v2/iso/keymaps/ahouseago/keymap.c
+++ keyboards/hs60/v2/iso/keymaps/ahouseago/keymap.c
@@ -19,7 +19,7 @@
 
 // Hyper when held, escape when pressed.
 #define HYP_ESC MT(MOD_RGUI, KC_ESC)
-#define MEN_MO3 LT(3, KC_APP)
+#define MEN_MO4 LT(4, KC_APP)
 
 // These are for discord mappings
 #define ALT_HOM RALT(KC_HOME)
@@ -32,30 +32,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,
     HYP_ESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS, KC_ENT,
     KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,           KC_RSFT,
-    KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 MO(1),    MO(2)  ,  MEN_MO3,  MO(4)),
+    KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 MO(2),    MO(3)  ,  MEN_MO4,  MO(5)),
 
-[1] = LAYOUT_60_iso( /* FN */
+[1] = LAYOUT_60_iso( /* COLEMAK */
+    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,  KC_BSPC,
+    KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,  KC_RBRC,
+    HYP_ESC,  KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,  KC_NUHS, KC_ENT,
+    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,     KC_NUBS,  KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,           KC_RSFT,
+    KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 MO(2),    MO(3)  ,  MEN_MO4,  MO(5)),
+
+[2] = LAYOUT_60_iso( /* FN */
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,
     _______,  KC_BTN1,  KC_MS_U,  KC_BTN2,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RESET  ,
     _______,  KC_MS_L,  KC_MS_D,  KC_MS_R,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,  _______,  _______,
     _______,  _______,  KC_BTN3,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
     _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
 
-[2] = LAYOUT_60_iso(
+[3] = LAYOUT_60_iso(
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  ALT_INS,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______,  _______,  _______,  ALT_HOM,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            PTT_RET,
     _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
 
-[3] = LAYOUT_60_iso(
-    RESET,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_INS,
+[4] = LAYOUT_60_iso(
+    RESET,    DF(0),    DF(1),    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_INS,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  _______,  _______,  S1_DEC,   S1_INC,   S2_DEC,   S2_INC,   _______,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  EF_DEC,   EF_INC,   H1_DEC,   H1_INC,   H2_DEC,   H2_INC,   BR_DEC,   BR_INC,   ES_DEC,   ES_INC,             _______,
     _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
 
-[4] = LAYOUT_60_iso(
+[5] = LAYOUT_60_iso(
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MPRV,  KC_MNXT,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MPLY,
