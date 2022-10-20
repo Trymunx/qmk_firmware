80ba5b71c5a69f2b69f54333aede9f7ad2a3a198
Alex Houseago <ahouseago@gmail.com>
2022-10-21 00:26:46 +0100

    Add a qwerty layer

--- keyboards/lily58/keymaps/ahouseago/keymap.c
+++ keyboards/lily58/keymaps/ahouseago/keymap.c
@@ -3,6 +3,7 @@
 enum layer_number {
   _COLEMAK = 0,
   _QWERTY,
+  _QWERTY_GAMING,
   _LOWER,
   _RAISE,
   _UPPER,
@@ -45,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,  KC_MINS,
   HYP_ESC,  KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                     KC_M,    KC_N,    KC_E,    KC_I,    KC_O,     KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V,DF(_QWERTY),KC_NUBS, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                        KC_LCTL, KC_LGUI, MO(_LOWER), KC_BSPC, KC_ENT,LT(_UPPER,KC_SPC), MO(_RAISE), KC_LALT
+                        KC_LCTL, KC_LALT, MO(_LOWER), KC_BSPC, KC_ENT,LT(_UPPER,KC_SPC), MO(_RAISE), KC_LGUI
 ),
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -65,9 +66,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_CAPS,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,DF(_COLEMAK),KC_RBRC,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                        KC_LCTL, KC_LALT, KC_SPC, KC_BSPC, KC_ENT, KC_SPC, MO(_RAISE), KC_LALT
+  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,KC_NUBS,DF(_QWERTY_GAMING),KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+                        KC_LCTL, KC_LALT, MO(_LOWER), KC_BSPC, KC_ENT, KC_SPC, MO(_RAISE), KC_LGUI
+),
+/* QWERTY
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LCtl | LGUI |LOWER | /BckSpac/       \Enter \  |Space |RAISE | LAlt |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+
+ [_QWERTY_GAMING] = LAYOUT(
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  KC_TAB,   KC_G,   KC_Q,    KC_W,    KC_E,    KC_R,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+  KC_ESC,   KC_B,   KC_A,    KC_S,    KC_D,    KC_F,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LCTL,  KC_LSFT,KC_Z,    KC_X,    KC_C,    KC_V, KC_T, DF(_COLEMAK), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+                        KC_LCTL, KC_LALT, MO(_LOWER), KC_SPC, KC_ENT, KC_SPC, MO(_RAISE), KC_LALT
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
