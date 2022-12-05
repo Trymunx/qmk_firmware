4736edfe58ec2a83b930f63b30c147533eaa4d7b
Alex Houseago <ahouseago@gmail.com>
2022-12-05 16:29:32 +0000

    Swap numbers and symbols

--- keyboards/lily58/keymaps/ahouseago/keymap.c
+++ keyboards/lily58/keymaps/ahouseago/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,KC_NUBS,DF(_QWERTY_GAMING),KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, _______,  KC_NUBS,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                         KC_LCTL, KC_LALT, MO(_LOWER), KC_BSPC, KC_ENT, KC_SPC, MO(_RAISE), KC_LGUI
 ),
 /* QWERTY
@@ -108,9 +108,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_NUHS,
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_COLN,
-  _______, MOD_UND, MOD_CUT, MOD_CPY, MOD_EXT, MOD_PST,LCTL(KC_B),_______,LSFT(KC_NUHS), KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NUHS,
+  KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_COLN,
+  _______, MOD_UND, MOD_CUT, MOD_CPY, MOD_EXT, MOD_PST,LCTL(KC_B),_______,LSFT(KC_NUHS), KC_UNDS, KC_PLUS,_______,_______,_______,
                              _______, _______, _______, _______, _______,  _______, _______, _______
 ),
 /* RAISE
@@ -129,7 +129,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, KC_PSCR,
+  _______,   DF(0),   DF(1),   DF(2), _______, _______,                     _______, _______, _______, _______, _______, KC_PSCR,
   _______, _______, _______, KC_LCBR, KC_RCBR, KC_COLN,                     KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MUTE, ALT_INS,
   _______, _______, _______, KC_LPRN, KC_RPRN, KC_EQL,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, PTT_RET, ALT_HOM,
   _______, _______, _______, KC_LBRC, KC_RBRC, _______,  _______, KC_MPLY,  KC_PLUS, KC_MINS, KC_LBRC, KC_RBRC, KC_EQL,  KC_BSLS,
