90621d2c4e1989d9015294ee658e838c9950dafb
Alex Houseago <ahouseago@gmail.com>
2024-03-11 11:12:14 +0000

    hs60: enter on lower+backspace(space)

--- keyboards/hs60/v2/iso/keymaps/ahouseago/keymap.c
+++ keyboards/hs60/v2/iso/keymaps/ahouseago/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,  _______,
     _______,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_COLN,  _______,  _______,
     _______,  CTLT_BS,  MOD_CUT,  MOD_CPY,  MOD_EXT,  MOD_PST, LCTL(KC_B),_______,  _______,  _______,  _______,  _______,            _______,
-    _______,  _______,  _______,                             LALT(KC_SPACE),                            _______,  _______,  _______,  _______),
+    _______,  _______,  _______,                                KC_ENT,                                 _______,  _______,  _______,  _______),
 };
 
 // Transparent layer -- copy to create a new one
